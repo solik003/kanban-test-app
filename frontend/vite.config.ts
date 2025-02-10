bb1762b2c8033f21f27e72b0e11c08979a2c5318
solik003 <malugasola@gmail.com>
2025-02-10 14:40:44 +0200

    Fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: 'kanban-test-app',
   build: {
-    outDir: 'dist'
+    outDir: 'build'
   },
   plugins: [react()],
-
 })
